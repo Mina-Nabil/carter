d026d1fa2ed91f225c80a67c1d11c355b97cc88b
Mina-Nabil <mina9492@hotmail.com>
2018-02-01 13:45:56 +0200

    LiveLine Api Update

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -295,7 +295,7 @@ class Api extends CI_Controller{
     $Lines = $this->LiveLines_model->getAvailableLines($LineID, $StartStation, $EndStation);
     $Indicies = $this->Paths_model->getPathIndicies($LineID, $StartStation, $EndStation);
 
-    if(!isset($Lines['FullLines'][0])) {
+    if(!isset($Lines['FullLines'])) {
       echo json_encode(array(
         'Response' => 'NoLines'
       ));
