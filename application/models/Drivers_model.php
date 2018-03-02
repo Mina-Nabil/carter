9bd8c975b451061871cf7a66fd89710956a25913
Mina-Nabil <mina9492@hotmail.com>
2018-03-02 07:46:50 +0200

    Driver Api

--- application/models/Drivers_model.php
+++ application/models/Drivers_model.php
@@ -172,7 +172,8 @@ class Drivers_model extends CI_Model{
                             WHERE LVLN_LINE_ID = LINE_ID
                             AND LVLN_DRVR_ID = DRVR_ID
                             AND LVLN_COMP = 0
-                            AND LVLN_TIME > DATE_ADD(NOW(), INTERVAL -1 DAY)
+                            AND LVLN_TIME < DATE_ADD(NOW(), INTERVAL 1 DAY)
+                            AND LVLN_TIME > DATE_ADD(NOW(), INTERVAL -1 HOUR)
                             AND DRVR_ID = {$DriverID}
                             ORDER BY LVLN_TIME ";
 
