92abcbdd73cbcba34688365dc7a8e1301e5f0aaa
Mina Nabil <mina9492@hotmail.com>
2018-03-30 14:20:37 +0200

    Check Promos Api

--- application/models/Promos_model.php
+++ application/models/Promos_model.php
@@ -49,15 +49,15 @@ class Promos_model extends CI_Model{
           $PromoArr = $this->getPromo_byCode($PromoCode)[0];
           if(!isset($PromoArr)) return array('PromoStatus' => 0); //Wrong Code
           $ExpiryType = $PromoArr['PRMO_TYPE'];
-          if(strcmp($ExpiryType, 'Date')){ //Expires by Date
+          if(strcmp($ExpiryType, 'Date') == 0){ //Expires by Date
             $Expiry = strtotime($PromoArr['PRMO_EXPIRE']);
             if($Expiry > date("Y-m-d H:i:s")) return array('PromoStatus' => 2); //Code Expired
           }
           $Used = $this->PromoUsageCount($PromoArr['PRMO_ID']);
-          if(strcmp($ExpiryType, 'Count')){ //Expires by Count
+          if(strcmp($ExpiryType, 'Count') == 0){ //Expires by Count
             if($Used >= $PromoArr['PRMO_CNT']) return array('PromoStatus' => 3); //Code Count Surpassed
           }
-          if(strcmp($ExpiryType, 'Both')){ //Expires by Both Count and Date
+          if(strcmp($ExpiryType, 'Both') == 0){ //Expires by Both Count and Date
             $Expiry = strtotime($PromoArr['PRMO_EXPIRE']);
             if($Expiry > date("Y-m-d H:i:s") || $Count >= $PromoArr['PRMO_CNT'] ) return array('PromoStatus' => 4); //Code Expired
           }
