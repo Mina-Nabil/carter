c812a1ea26dd6b9e93d8b58de802f90c8a9db60c
Mina Nabil <mina9492@hotmail.com>
2018-03-30 14:15:03 +0200

    Check Promos Api

--- application/models/Drivers_model.php
+++ application/models/Drivers_model.php
@@ -192,7 +192,6 @@ class Drivers_model extends CI_Model{
                             AND LVLN_DRVR_ID = DRVR_ID
                             AND LVLN_COMP = 1
                             AND LVLN_TIME > DATE_ADD(NOW(), INTERVAL -36 DAY)
-                            AND LVLN_TIME <= NOW()
                             AND DRVR_ID = {$DriverID}
                             ORDER BY LVLN_TIME ";
 
