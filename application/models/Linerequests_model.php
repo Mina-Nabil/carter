cd16bc4e9a4d79a9cf683fce2a54230914a27a4e
Mina Nabil <mina9492@hotmail.com>
2018-04-13 20:28:29 +0200

    Api Modifications

--- application/models/Linerequests_model.php
+++ application/models/Linerequests_model.php
@@ -12,32 +12,13 @@ class Linerequests_model extends CI_Model{
         public function getLinerequests(){
 
           $strSQL = "SELECT LNRQ_ID, LNRQ_SEATS, LNRQ_TWO_WAYS, LNRQ_CLNT_ID, CLNT_NAME, LNRQ_BACK_TIME,
-                            LNRQ_START_TIME, LNRQ_NOTES, LNRQ_START_STTN, LNRQ_END_STTN, STTN_NAME as StartStation
-                      FROM Linerequests, clients, stations
+                            LNRQ_START_TIME, LNRQ_NOTES, LNRQ_START_STTN, LNRQ_END_STTN
+                      FROM Linerequests, clients
                       WHERE LNRQ_CLNT_ID = CLNT_ID
-                      AND   LNRQ_START_STTN = STTN_ID
                       ORDER BY LNRQ_ID";
           $query = $this->db->query($strSQL);
-          $res1 =  $query->result_array();
+          $res =  $query->result_array();
 
-          $strSQL = "SELECT LNRQ_ID, LNRQ_END_STTN, STTN_NAME as EndStation
-                    FROM Linerequests, stations
-                    WHERE LNRQ_END_STTN = STTN_ID";
-          $query = $this->db->query($strSQL);
-          $res2 =  $query->result_array();
-
-          $res = array();
-          $i=0;
-
-          foreach($res1 as $row){
-            $res[$i] = $row;
-            $i++;
-          }
-          $i=0;
-          foreach($res2 as $row){
-            $res[$i] = array_merge($res[$i], $row);
-            $i++;
-          }
           return $res;
 
         }
@@ -45,35 +26,14 @@ class Linerequests_model extends CI_Model{
         public function getLinerequest_byID($ID){
 
           $strSQL = "SELECT LNRQ_ID, LNRQ_SEATS, LNRQ_TWO_WAYS, LNRQ_CLNT_ID, CLNT_NAME, LNRQ_BACK_TIME,
-                            LNRQ_START_TIME, LNRQ_NOTES, LNRQ_START_STTN, LNRQ_END_STTN, STTN_NAME as StartStation
+                            LNRQ_START_TIME, LNRQ_NOTES, LNRQ_START_STTN, LNRQ_END_STTN
                       FROM Linerequests, clients, stations
                       WHERE LNRQ_CLNT_ID = CLNT_ID
-                      AND   LNRQ_START_STTN = STTN_ID
                       AND   LNRQ_ID = {$ID}
                       ORDER BY LNRQ_ID";
           $query = $this->db->query($strSQL);
-          $res1 =  $query->result_array();
+          $res =  $query->result_array();
 
-          $strSQL = "SELECT LNRQ_ID, LNRQ_END_STTN, STTN_NAME as EndStation
-                    FROM Linerequests, stations
-                    WHERE LNRQ_END_STTN = STTN_ID
-                    AND   LNRQ_ID = {$ID}";
-
-          $query = $this->db->query($strSQL);
-          $res2 =  $query->result_array();
-
-          $res = array();
-          $i=0;
-
-          foreach($res1 as $row){
-            $res[$i] = $row;
-            $i++;
-          }
-          $i=0;
-          foreach($res2 as $row){
-            $res[$i] = array_merge($res[$i], $row);
-            $i++;
-          }
           return $res;
 
 
